05ec310b4fed908207f4efbfe481d731e26cb36f
ratonazul <correofalso@diegolopez.eu>
2026-02-14 16:28:29 +0100

    🛠️ init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import './layout.css';
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="theme-gruvbox w-full min-h-screen bg-bg0 text-fg0">
+	{@render children()}
+</div>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a
+			href={localizeHref(page.url.pathname, { locale })}
+		>
+			{locale}
+		</a>
+	{/each}
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="bg-red text-bg0 font-bold">Gruvbox</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
