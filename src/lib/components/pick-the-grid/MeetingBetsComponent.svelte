d5f9b61520b565a13554c8604623fca2eee5bfc9
ratonazul <correofalso@diegolopez.eu>
2026-05-24 17:41:46 +0200

    🛠 make time font size smaller, fix time bet points color

--- src/lib/components/pick-the-grid/MeetingBetsComponent.svelte
+++ src/lib/components/pick-the-grid/MeetingBetsComponent.svelte
@@ -12,16 +12,18 @@
 
 </script>
 
-{#if meetingBets.isLoading || meetingBets.isLoadingError}
-    <section class="bg-bg1 animate-pulse sm:h-17 h-26 w-full mb-8"></section>
-{/if}
-{#if meetingBets.isSuccess && meetingBets.data.users.length !== 0}
-    <div class="mb-8">
-        <CustomH2>pick results</CustomH2>
-        <div class="flex gap-8 overflow-x-scroll">
-            {#each meetingBets.data.users as user, i}
-                <MiniBetComponent meetingBets={user} {drivers} position={i+1} />
-            {/each}
+<div class="mb-8">
+    <CustomH2>pick results</CustomH2>
+    {#if meetingBets.isLoading || meetingBets.isLoadingError}
+        <section class="bg-bg1 animate-pulse sm:h-17 h-26 w-full mb-8"></section>
+    {/if}
+    {#if meetingBets.isSuccess && meetingBets.data.users.length !== 0}
+        <div class="mb-8">
+            <div class="flex gap-8 overflow-x-scroll">
+                {#each meetingBets.data.users as user, i}
+                    <MiniBetComponent meetingBets={user} {drivers} position={i+1} />
+                {/each}
+            </div>
         </div>
-    </div>
-{/if}
+    {/if}
+</div>
